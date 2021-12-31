7cb152529219d86a8133b8c23b5bf67a7b35df5a
ldsleticia <lds.leticia.dos.santos@gmail.com>
2021-12-31 13:23:03 -0300

    Novos exercícios

---
+++ exercicios _de_aula/classes_e_objetos/oop.py
@@ -0,0 +1,16 @@
+import datetime
+class Pessoa():
+    def __init__(self, nome, nascimento):
+        self.nome = nome
+        self.nascimento = nascimento
+    def idade(self):
+        delta = datetime.date.today() - self.nascimento
+        return int(delta.days/365)
+    def __str__(self):
+        return f'{self.nome}, {self.idade()} anos'
+santos = Pessoa('Letícia', datetime.date(1996, 4, 16))
+masanori = Pessoa('Fernando', datetime.date(1980,9,1))
+print(santos.idade())
+print(santos)
+print(masanori.idade())
+print(masanori)
